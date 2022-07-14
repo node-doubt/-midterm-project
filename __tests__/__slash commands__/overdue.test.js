5287002ad8778669f01239bbf216f7a106b5390c
bc0351 <benchoe@2six.org>
2022-07-14 18:56:22 -0400

    test

--- __tests__/__slash commands__/overdue.test.js
+++ __tests__/__slash commands__/overdue.test.js
@@ -2,7 +2,7 @@
 
 const overdue = require('../../src/slash-commands/overdue');
 
-describe('duetoday()', () => {
+describe('overdue()', () => {
   it('should successfully send a request to CANVAS API for assignments due today.', () => {
     expect(() => { overdue() }).toBeTruthy();
     
