85519c8bc358682736b6b65abd35eb3ed19a5ffa
Cody Davis <davis.r.cody@gmail.com>
2022-07-13 19:16:44 -0700

    overdue route

---
+++ src/slash-commands/overdue.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const axios = require('axios');
+require('dotenv').config();
+
+let token = process.env.CANVAS_TOKEN;
+
+let courseId = process.env.CANVAS_COURSE_ID;
+
+const overdue = async ({ command, ack, say }) => {
+  // Acknowledge command request
+  await ack();
+  const config = {
+    headers: {
+      // Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  let response = await axios.get(
+      `https://canvas.instructure.com/api/v1/courses/${courseId}/assignments?bucket=overdue`,
+      config,
+  );
+
+  const overdueAssignments = response.data
+
+  let str = '`Overdue Assignments`\n';
+  overdueAssignments.forEach((item) => {
+    str += item.name + '\n' + item.html_url + '\n';
+  });
+
+  await say(str);
+};
+
+module.exports = overdue;
