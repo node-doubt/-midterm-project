09c162a3b5490e617831981567c89abafddb04c6
Katharine Swilley <kswilley@gmail.com>
2022-07-14 08:20:14 -0700

    added sequelize dialect options

--- src/userModel/index.js
+++ src/userModel/index.js
@@ -8,16 +8,16 @@ const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite::memory'
   : process.env.DATABASE_URL || 'sqlite::memory';
 
-// const sequelize = new Sequelize(DATABASE_URL, {
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });   
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});   
 
-const sequelize = new Sequelize(DATABASE_URL);
+// const sequelize = new Sequelize(DATABASE_URL);
 
 const UserModel = userSchema(sequelize, DataTypes);
 
